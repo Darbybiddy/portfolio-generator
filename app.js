1bba7e2e322b65ce0e1f96f71aa2af7a62f096eb
Darby Biddy <darbybiddy3@gmail.com>
2022-10-10 17:42:25 -0500

    added my first question and answer

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-const fs = require('fs')
-const generatePage = require('./src/page-template')
+const inquirer = require('inquirer');
+// const fs = require('fs')
+// const generatePage = require('./src/page-template')
+ 
+inquirer
+.prompt ([
+  {
+  type: 'input',
+  name: 'name',
+  message: 'What is your name?'
+}
+])
+.then(answers => console.log(answers))
 
-const profileDataArgs = process.argv.slice(2);
-//const name = profileDataArgs[0] lines 2 and 3 are a different way of writing line 4, but is the same thing
-//const github = profileDataArgs[1]
-const [name, github] = profileDataArgs
+// // const profileDataArgs = process.argv.slice(2);
+// // //const name = profileDataArgs[0] lines 2 and 3 are a different way of writing line 4, but is the same thing
+// // //const github = profileDataArgs[1]
+// // const [name, github] = profileDataArgs
 
-const pageHTML = generatePage(name, github)
-// the first thing in the argument is going to be the file name, (index.html)
-//the second argument is the data thats being written (generatePage)(everything that is in the generatepage function)
-// the third argument is the callback funtion that handles any errors and success messages. 
-fs.writeFile('index.html',pageHTML, err =>{
-  if (err)throw err
+// const pageHTML = generatePage(name, github)
+// // the first thing in the argument is going to be the file name, (index.html)
+// //the second argument is the data thats being written (generatePage)(everything that is in the generatepage function)
+// // the third argument is the callback funtion that handles any errors and success messages. 
+// fs.writeFile('index.html',pageHTML, err =>{
+//   if (err)throw err
 
-console.log('portfolio complete! checkout index.html to see the output!');
-})
+// console.log('portfolio complete! checkout index.html to see the output!');
+// })
 
 
