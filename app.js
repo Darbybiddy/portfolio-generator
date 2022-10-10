fdf180c14acc8cda8793bb95bba3f1aec498ad4e
Darby Biddy <darbybiddy3@gmail.com>
2022-10-10 16:01:37 -0500

    made another page and exported the html file

--- app.js
+++ app.js
@@ -1,38 +1,19 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs')
+const generatePage = require('./src/page-template')
+
+const profileDataArgs = process.argv.slice(2);
 //const name = profileDataArgs[0] lines 2 and 3 are a different way of writing line 4, but is the same thing
 //const github = profileDataArgs[1]
 const [name, github] = profileDataArgs
 
-// this function will recieve the command line argument and inserst them in a html termplate literal
-const generatePage = (userName, githubName) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
+const pageHTML = generatePage(name, github)
+// the first thing in the argument is going to be the file name, (index.html)
+//the second argument is the data thats being written (generatePage)(everything that is in the generatepage function)
+// the third argument is the callback funtion that handles any errors and success messages. 
+fs.writeFile('index.html',pageHTML, err =>{
+  if (err)throw err
 
-  <body>
-  <h1>${name}</h1>
-  <h2><a href="https://github.com/${github}">Github</a></h2>
-</body>
-</html>
-`
-};
- console.log(name, github)
-console.log(generatePage(name,github));
+console.log('portfolio complete! checkout index.html to see the output!');
+})
 
 
-// console.log(profileDataArgs)
-// const printProfileData = (profileDataArr)=>{
-//     for (let i = 0; i < profileDataArr.length; i+=1){
-//        console.log(profileDataArr[i])
-//     }
-//    console.log('============')
-// //is the same as this...
-// profileDataArr.forEach(profileItem => console.log(profileItem))
-// }
-//printProfileData(profileDataArgs)
