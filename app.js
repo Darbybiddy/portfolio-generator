0e6927d32accf634321ba1ed9078c086c4400bcc
Darby Biddy <darbybiddy3@gmail.com>
2022-10-10 21:18:25 -0500

    added questions and pormpts

--- app.js
+++ app.js
@@ -1,16 +1,103 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 // const fs = require('fs')
 // const generatePage = require('./src/page-template')
- 
-inquirer
-.prompt ([
-  {
-  type: 'input',
-  name: 'name',
-  message: 'What is your name?'
-}
-])
-.then(answers => console.log(answers))
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+
+    {
+      type: "input",
+      name: "github",
+      message: "What is your Github Username?",
+    },
+
+    {
+      type: "input",
+      name: "about",
+      message: "Provide some information about yourself:",
+    },
+  ]);
+};
+
+const promptProject = (portfolioData) => {
+  console.log(`
+  ===================
+   Add a new Project
+  ===================
+  `);
+
+  // If there's no 'projects' array property, create one
+  if (!portfolioData.projects) {
+  portfolioData.projects = [];
+  }
+
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "what is the name of your project?",
+    },
+
+    {
+      type: "input",
+      name: "description",
+      message: "provide a description of the project (required)",
+    },
+
+    {
+      type: "checkbox",
+      name: "languages",
+      message: "What did you build this project with? (check all that apply)",
+      choices: [
+        "JavaScript",
+        "HTML",
+        "CSS",
+        "ES6",
+        "jQuery",
+        "Bootstrap",
+        "Node",
+      ],
+    },
+
+    {
+      type: "input",
+      name: "link",
+      message: "enter the Github link to your project. (Required)",
+    },
+
+    {
+      type: "confirm",
+      name: "feature",
+      message: "would you like to feature this project?",
+      default: false,
+    },
+
+    {
+      type: "confirm",
+      name: "confirmAddProject",
+      message: "would you like to enter another project?",
+      default: false,
+    },
+  ])
+  .then(projectData => {
+    portfolioData.projects.push(projectData);
+    if (projectData.confirmAddProject) {
+      return promptProject(portfolioData);
+    } else {
+      return portfolioData;
+    }
+  });
+};
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData)
+  })
 
 // // const profileDataArgs = process.argv.slice(2);
 // // //const name = profileDataArgs[0] lines 2 and 3 are a different way of writing line 4, but is the same thing
@@ -20,11 +107,9 @@ inquirer
 // const pageHTML = generatePage(name, github)
 // // the first thing in the argument is going to be the file name, (index.html)
 // //the second argument is the data thats being written (generatePage)(everything that is in the generatepage function)
-// // the third argument is the callback funtion that handles any errors and success messages. 
+// // the third argument is the callback funtion that handles any errors and success messages.
 // fs.writeFile('index.html',pageHTML, err =>{
 //   if (err)throw err
 
 // console.log('portfolio complete! checkout index.html to see the output!');
 // })
-
-
